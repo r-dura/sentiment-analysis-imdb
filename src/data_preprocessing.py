c86a8ce0ab78f1f8c01ad76ccd97cb9e29a93c4b
Raj Duraisamy <raj.duraisamy03@gmail.com>
2024-08-26 14:18:09 -0700

    1. implemented predict function with gaussian likelihood 2. refactored naive bayes model 3. reduced data leakage by building filtered bag of words on only train instead of both train and test

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -20,7 +20,7 @@ def tokenize_and_clean(text):
     return tokens
 
 # Building bag of words for all reviews
-def build_full_bow(reviews):
+def build_bow(reviews):
     full_bow = Counter()
     for review in reviews:
         tokens = tokenize_and_clean(review)
@@ -28,24 +28,37 @@ def build_full_bow(reviews):
         full_bow.update(bow)
     return full_bow
 
-# Tokenizes reviews and filters out word not present in total bag of words
-def preprocess_reviews(reviews, full_bow):
+# Tokenizes each review in data set given the total bag of words from dataset
+def preprocess_reviews(reviews, bow):
     processed_reviews = []
     for review in reviews:
         tokens = tokenize_and_clean(review)
-        filtered_tokens = [word for word in tokens if word in full_bow]
+        filtered_tokens = [word for word in tokens if word in bow]
         processed_review = ' '.join(filtered_tokens)
         processed_reviews.append(processed_review)
     return processed_reviews
 
-# Converts the vocabulary to a fixed-length vector with size of vocab (17738) + 1 elements
 def prepare_data(df, test_size=0.2, random_state=42):
-    full_bow = build_full_bow(df['review'])
-    processed_reviews = preprocess_reviews(df['review'], full_bow)
+    # split data into training and testing
+    train_df, test_df = train_test_split(df, test_size=test_size, random_state=random_state, stratify=df['sentiment'])
+
+    # Filter bow only using training data to keep tokens with a min occurence greater than 2
+    train_bow = build_bow(train_df['review'])
+    filtered_bow = {token: train_bow[token] for token in train_bow if train_bow[token] > 10}
+
+    # Process training and testing reviews
+    processed_train_reviews = preprocess_reviews(train_df['review'], filtered_bow)
+    processed_test_reviews = preprocess_reviews(test_df['review'], filtered_bow)
+
+    # Vectorize the data
     tokenizer = Tokenizer()
-    tokenizer.fit_on_texts(processed_reviews)
-    # encode training data set
-    X = tokenizer.texts_to_matrix(processed_reviews, mode='freq')
-    y = df['sentiment'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state, stratify=y)
+    tokenizer.fit_on_texts(processed_train_reviews)
+    
+    # encode training and testing data sets
+    X_train = tokenizer.texts_to_matrix(processed_train_reviews, mode='freq')
+    y_train = train_df['sentiment'].values
+
+    X_test = tokenizer.texts_to_matrix(processed_test_reviews, mode='freq')
+    y_test = test_df['sentiment'].values
+
     return X_train, X_test, y_train, y_test
---
+++ src/naive_bayes_model.py
@@ -0,0 +1,60 @@
+import numpy as np
+import math
+
+class NaiveBayes:
+    def __init__(self):
+        self.unique_classes = None
+        self.priors = None
+        self.means = None
+        self.variances = None
+        
+    def fit(self, X, y):
+        """Trains model on data. Calculates means, variances, and priors for each class"""
+        self.unique_classes = np.unique(y)
+        num_classes = len(self.unique_classes)
+        num_features = X.shape[1]
+
+        self.means = np.zeros((num_classes, num_features))
+        self.variances = np.zeros((num_classes, num_features))
+        for idx, c in enumerate(self.unique_classes):
+            X_c = X[y == c]  # All reviews of class c
+            self.means[idx] = X_c.mean(axis=0) # Calculates the mean of the column
+            self.variances[idx] = X_c.var(axis=0)
+        
+        # Add small epsilon to variances to prevent division by zero
+        self.variances = np.maximum(self.variances, 1e-10)
+            
+        self.priors = self.calculate_prior_probs(y)
+            
+
+    def calculate_likelihood(self, x, mean, var):
+        """Calculates the Gaussian likelihood of the data with the given mean and variance"""
+        return  -0.5 * (np.log(2 * np.pi * var) + ((x - mean) ** 2) / var)
+        
+
+    def predict(self, X):
+        """Performs inference using Bayes' Theorem: P(A | B) = P(B | A) * P(A) / P(B)
+        We have to add the log-likelihoods given each feature"""
+        predictions = []
+        # For each sample, we have to take each feature(word) in the sample and get the likelihood
+        for x in X:
+            class_scores = []
+            # we must do this for each class, then find the highest score for each sample and classify
+            for class_idx, c in enumerate(self.unique_classes):
+                prior = np.log(self.priors[class_idx])
+                log_likelihood = np.sum(self.calculate_likelihood(x, self.means[class_idx], self.variances[class_idx]))
+                posterior = prior + log_likelihood
+                class_scores.append(posterior)
+            predicted_class = np.argmax(class_scores)
+            predictions.append(self.unique_classes[predicted_class])
+        return predictions
+
+    def calculate_prior_probs(self, y):
+        """Calculate proportion of words taken up by each class"""
+        unique, counts = np.unique(y, return_counts=True)
+        total_samples = len(y)
+        num_classes = len(unique)
+        prior_probs = np.zeros(num_classes)
+        for idx, c in enumerate(unique):
+            prior_probs[idx] = counts[idx] / total_samples
+        return prior_probs
